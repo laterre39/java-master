560b897799a66b2b0f1b77262f5f31e3541e6971
newworld <newworld-@naver.com>
2023-07-24 18:59:16 +0900

    feat: 백만년만의 커밋... 깃 안올렸더니 기존 자료 유실됬다 ㅠ...

---
+++ src/BasicJava/Chapter_09/ExEquals1.java
@@ -0,0 +1,30 @@
+package BasicJava.Chapter_09;
+
+public class ExEquals1 {
+    public static void main(String[] args) {
+        Value v1 = new Value(10);
+        Value v2 = new Value(10);
+
+        if (v1.equals(v2)) { // 같은 객체를 참조하고 있는지 검사한다.
+            System.out.println("v1과 v2는 같습니다.");
+        }
+        else
+            System.out.println("v1과 v2는 다릅니다."); // 서로 다른 객체이기 때문에 false 결과를 얻는다.
+
+        v2 = v1;
+
+        if (v1.equals(v2)) {
+            System.out.println("v1과 v2는 같습니다."); // 참조 객체를 동일하게 해 true 결과를 얻는다.
+        }
+        else
+            System.out.println("v1과 v2는 다릅니다.");
+    }
+}
+
+class Value {
+    int value;
+
+    Value(int value) {
+        this.value = value;
+    }
+}
---
+++ src/BasicJava/Chapter_09/ExEquals2.java
@@ -0,0 +1,35 @@
+package BasicJava.Chapter_09;
+
+class Person {
+    long id;
+
+    public boolean equals(Object obj) {  // equals 오버라이딩
+        if (obj instanceof Person) { // 객체가 person 을 참조하면 객체의 id를 비교한다.
+            return id == ((Person)obj).id;
+        }
+        else return false;
+    }
+
+    Person(long id) {
+        this.id = id;
+    }
+}
+public class ExEquals2 {
+    public static void main(String[] args) {
+        Person p1 = new Person(1234123422L);
+        Person p2 = new Person(1234123422L);
+
+        if (p1 == p2) { // 인스턴스가 다르기 때문에 false
+            System.out.println("p1과 p2는 같습니다.");
+        }
+        else
+            System.out.println("p1과 p2는 다릅니다.");
+
+
+        if (p1.equals(p2)) { // 인스턴스가 다르지만 오버라이딩 된 equals 에 따라 값을 비교하기 때문에 true
+            System.out.println("p1과 p2는 같습니다.");
+        }
+        else
+            System.out.println("p1과 p2는 다릅니다.");
+    }
+}
